028fcce296360068535823cb0c01d36af111632b
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-10-12 14:20:59 -0300

    feat: add dropdown in html

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
 import './styles/index.scss'
+import Dropdown from './Dropdown'
 
+const mainElement = document.querySelector('main')
+
+const dropdown = new Dropdown()
+dropdown.addButton('Tipo de Desconto')
+dropdown.addItem('Pix')
+dropdown.addItem('Credenciada')
+dropdown.addItem('Exames de Lab')
+dropdown.addItem('Exames de Imagem')
+dropdown.appendIn(mainElement)
