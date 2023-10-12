36f581f496ff837ec1c76bc26fe69124db33b2ee
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-10-12 16:27:28 -0300

    feat: instancied title

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import './styles/index.scss'
 import Dropdown from './Dropdown'
+import Title from './Title'
 
+const headerElement = document.querySelector('header .container') as HTMLElement
 const mainElement = document.querySelector('main .container') as HTMLElement
+
+const title = new Title()
+title.addTitle('Calculadora de Desconto')
+title.appendIn(headerElement)
 
 const dropdown = new Dropdown()
 dropdown.addButton('Tipo de Desconto')
