e27a369024ff8d2af6c5dbe5729b3bd2947b2767
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-10-12 17:25:37 -0300

    feat: instancied paragraph

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import './styles/index.scss'
 import Dropdown from './Dropdown'
 import Title from './Title'
 import Input from './Input'
+import Paragraph from './Paragraph'
 
 const headerElement = document.querySelector('header .container') as HTMLElement
 const mainElement = document.querySelector('main .container') as HTMLElement
@@ -19,6 +20,34 @@ dropdown.addItem('Exames de Imagem')
 dropdown.appendIn(mainElement)
 
 const input = new Input()
-input.addLabel('Valor do procedimento','procedure')
+input.addLabel('Valor do procedimento', 'procedure')
 input.addField('procedure')
 input.appendIn(mainElement)
+
+const paragraph = new Paragraph()
+paragraph.addText('Tipo de desconto: ')
+paragraph.addSpan('Pix')
+paragraph.appendIn(mainElement)
+
+paragraph.newInstance()
+paragraph.addText('Porcentagem de desconto: ')
+paragraph.addSpan('30%')
+paragraph.appendIn(mainElement)
+
+paragraph.newInstance()
+paragraph.addText('Valor do procedimento: ')
+paragraph.addSpan('R$ ')
+paragraph.addSpan('360,00')
+paragraph.appendIn(mainElement)
+
+paragraph.newInstance()
+paragraph.addText('Desconto a ser aplicado: ')
+paragraph.addSpan('R$ ')
+paragraph.addSpan('252,00')
+paragraph.appendIn(mainElement)
+
+paragraph.newInstance()
+paragraph.addText('Procedimento com desconto: ')
+paragraph.addSpan('R$ ')
+paragraph.addSpan('108,00')
+paragraph.appendIn(mainElement)
