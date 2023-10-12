92c0c050815781da3ffa0cab5fdf5420bfcd2fa2
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-10-12 16:56:15 -0300

    feat: instancied input

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import './styles/index.scss'
 import Dropdown from './Dropdown'
 import Title from './Title'
+import Input from './Input'
 
 const headerElement = document.querySelector('header .container') as HTMLElement
 const mainElement = document.querySelector('main .container') as HTMLElement
@@ -16,3 +17,8 @@ dropdown.addItem('Credenciada')
 dropdown.addItem('Exames de Lab')
 dropdown.addItem('Exames de Imagem')
 dropdown.appendIn(mainElement)
+
+const input = new Input()
+input.addLabel('Valor do procedimento','procedure')
+input.addField('procedure')
+input.appendIn(mainElement)
