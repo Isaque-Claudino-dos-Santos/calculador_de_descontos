612fb9cb95b0a6d1e42229bad461ba56b6bf09e4
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-10-12 16:56:25 -0300

    feature: created input

---
+++ src/Input/Input.ts
@@ -0,0 +1,34 @@
+import ElementHandler from '../Modules/ElementHandler'
+
+export default class Input extends ElementHandler {
+  public readonly container: HTMLElement = document.createElement('div')
+  public readonly fixClassName: string = 'input'
+  public readonly labelClassName: string = `${this.fixClassName}_label`
+  public readonly inputClassName: string = `${this.fixClassName}_field`
+
+  constructor() {
+    super()
+    this.provideClassName()
+  }
+
+  private provideClassName() {
+    this.container.className = this.fixClassName
+  }
+
+  addLabel(text: string, htmlFor: string): void {
+    const label = document.createElement('label')
+    label.textContent = text
+    label.htmlFor = htmlFor
+    label.className = this.labelClassName
+    this.container.appendChild(label)
+  }
+
+  addField(id: string, type: string = 'text'): void {
+    const input = document.createElement('input')
+    input.type = type
+    input.id = id
+    input.className = this.inputClassName
+    input.onload = (e) => e.preventDefault()
+    this.container.appendChild(input)
+  }
+}
